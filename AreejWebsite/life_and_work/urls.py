6f4f41b1f20a5f366fa0be6f119a522bcb2f313b
Areej Shareefi <aomsh1420@gmail.com>
2024-11-14 10:26:53 +0300

    working on project

--- AreejWebsite/life_and_work/urls.py
+++ AreejWebsite/life_and_work/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name="life_and_work"
 
 urlpatterns=[
+    path('career/',views.career_view,name="career_view"),
+    path('freespace/',views.freespace_view,name="freespace_view"),
+    path('freespace/<int:interest_id>/interestdetail/',views.interest_detail_view,name="interest_detail_view"),
+    path('freespace/<int:memo_id>/memodetail/',views.memo_detail_view,name="memo_detail_view"),
+    path('freespace/<int:project_id>/projectdetail/',views.project_detail_view,name="project_detail_view"),
 
 ]
--- AreejWebsite/life_and_work/views.py
+++ AreejWebsite/life_and_work/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+
+def career_view(request:HttpRequest):
+
+   return render(request,"life_and_work/career.html")
+
+
+def freespace_view(request:HttpRequest):
+
+   return render(request,"life_and_work/freespace.html")
+
+
+def interest_detail_view(request:HttpRequest,interest_id):
+
+   return render(request,"life_and_work/interest_detail.html")
+
+
+def memo_detail_view(request:HttpRequest,memo_id):
+
+   return render(request,"life_and_work/memo_detail.html")
+
+
+def project_detail_view(request:HttpRequest,project_id):
+
+   return render(request,"life_and_work/project_detail.html")
